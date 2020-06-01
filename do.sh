68f09e71d94ded5c510b73dc716da3382bced9b8
Manabu Ori <manabu.ori@gmail.com>
2020-06-01 22:15:48 +0900

    Do all the stuff

---
+++ do.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ x"$#" !=  x"1" ]; then
+	echo "$0 output_dir"
+	exit
+fi
+output_dir=$1; shift
+
+mkdir -p ${output_dir}/misc
+oc -n openshift-cluster-version logs $(oc -n openshift-cluster-version get pod -o custom-columns=NAME:.metadata.name --no-headers) > ${output_dir}/misc/
+
+kubectl api-resources -o wide > ${output_dir}/misc/kubectl_api-resources_-o_wide
+
+top_confdir=${output_dir}/misc/router_configs
+mkdir -p ${top_confdir}
+
+for pod in $(oc -n openshift-ingress get pod -o=custom-columns=NAME:.metadata.name --no-headers); do
+	echo "=> ${pod}"
+	pod_confdir=${top_confdir}/${pod}
+	mkdir -p ${pod_confdir}
+	#for file in $(oc -n openshift-ingress rsh ${pod} ls -1 | cat); do
+	for file in $(kubectl -n openshift-ingress exec -it ${pod} ls); do
+		_file=$(echo ${file} | tr '\r' '\n')
+		echo "==> ${_file}"
+		oc -n openshift-ingress rsh ${pod} cat "${_file}" > ${pod_confdir}/${_file}
+	done
+done
+
+(time ./kubedump -d ${output_dir} -a) 2>&1 | tee ${output_dir}/misc/log.kubedump
